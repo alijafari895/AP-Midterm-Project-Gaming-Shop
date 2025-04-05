21f91737e68005cadb4d4acd1626c96e389011f7
alijafari895 <alijafari895@gmail.com>
2025-04-05 20:38:32 +0330

    show
    change show from virtual to void

--- code/main.cpp
+++ code/main.cpp
@@ -17,43 +17,25 @@ class product {
                         this->price = price;
                         this->amount = amount;
                 }
-                virtual void show() {
-                        cout << "نام: " << name << " | قیمت: " << price << " | موجودی: " << amount << endl;
+
+                void show() {
+                        cout << "Name: " << this->name << endl;
+                        cout << "Price: " << this->price << endl;
+                        cout << "Amount: " << this->amount << endl;
+                }
 
 };
 class games : public product {
         public:
                 games(string name, int price, double amount): product( name , price , amount ){}
-
-                void show() override {
-                        cout << " Game | " << name << " | " << price << " | " << amount << endl;
-                }
-
 };
 class consols : public product {
         public:
                 consols(string name, int price, double amount) : product( name , price , amount ) {}
-
-                void show() override {
-                        cout << " Consol | " << name << " | " << price << " | " << amount << endl;
-                }
-
 };
 class headphone : public product {
         public:
                 headphone(string name, int price, double amount) : product( name , price , amount ) {}
-
-                void show() override {
-                        cout << "Headphon | " << name << " | " << price << " | " << amount << endl;
-                }
 };
 
 
-
-
-
-
-
-int main() {
-
-}
