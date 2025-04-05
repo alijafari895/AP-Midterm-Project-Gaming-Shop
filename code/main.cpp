87a050c4ec2f5120c0e04124b7a8b2568506e422
alijafari895 <alijafari895@gmail.com>
2025-04-05 20:45:28 +0330

    add gamingshop class
    add gaimingshop class with some add functions and wallet

--- code/main.cpp
+++ code/main.cpp
@@ -3,39 +3,66 @@
 #include <string>
 using namespace std;
 
-class gamingshop {
-};
-
 class product {
-        protected:
-                string name;
-                int price;
-                double amount;
-        public:
-                product(string name, int price, double amount) {
-                        this->name = name;
-                        this->price = price;
-                        this->amount = amount;
-                }
+protected:
+    string name;
+    int price;
+    double quntity;
+public:
+    product(string name, int price, double quntity) {
+        this->name = name;
+        this->price = price;
+        this->quntity = quntity;
+    }
 
-                void show() {
-                        cout << "Name: " << this->name << endl;
-                        cout << "Price: " << this->price << endl;
-                        cout << "Amount: " << this->amount << endl;
-                }
+    void show() {
+        cout << "Name: " << this->name << endl;
+        cout << "Price: " << this->price << endl;
+        cout << "Quntity: " << this->quntity << endl;
+    }
 
 };
 class games : public product {
-        public:
-                games(string name, int price, double amount): product( name , price , amount ){}
+public:
+    games(string name, int price, double quntity): product( name , price , quntity ){}
 };
 class consols : public product {
-        public:
-                consols(string name, int price, double amount) : product( name , price , amount ) {}
+public:
+    consols(string name, int price, double quntity) : product( name , price , quntity ) {}
 };
 class headphone : public product {
-        public:
-                headphone(string name, int price, double amount) : product( name , price , amount ) {}
+public:
+    headphone(string name, int price, double quntity) : product( name , price , quntity ) {}
+};
+
+class gamingshop {
+private:
+    vector<games> all_games;
+    vector<consols> all_consols;
+    vector<headphone> all_headphones;
+    int wallet = 0;
+
+public:
+    void addGame(string name, int price, double amount) {
+        all_games.push_back(games(name, price, amount));
+    }
+
+    void addConsol(string name, int price, double amount) {
+        all_consols.push_back(consols(name, price, amount));
+    }
+
+    void addHeadphone(string name, int price, double amount) {
+        all_headphones.push_back(headphone(name, price, amount));
+    }
+
+    void chargeWallet(int amount) {
+        wallet += amount;
+        cout << "💳 کیف پول شارژ شد. موجودی فعلی: " << wallet << endl;
+    }
+
+    void showWallet() {
+        cout << "💰 موجودی کیف پول: " << wallet << endl;
+    }
 };
 
 
